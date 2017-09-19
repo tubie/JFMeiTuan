d4da1d829433eacf6fe95c66e013eb7f85390ec1
linjf <linjf@fenqi.im>
2017-09-19 10:13:05 +0800

    fix bug

--- JFMeiTuan/Class/ToolClass/GetUrlString.m
+++ JFMeiTuan/Class/ToolClass/GetUrlString.m
@@ -8,15 +8,36 @@
 #import "GetUrlString.h"
 #import "AppDelegate.h"
 #import "JFHomeServiceModel.h"
+static GetUrlString *shareUrl;
 @implementation GetUrlString
-+(GetUrlString *)sharedManager{
-    static GetUrlString *shareUrl = nil;
++(instancetype)sharedManager{
     static dispatch_once_t predicate;
     dispatch_once(&predicate, ^{
         shareUrl = [[self alloc]init];
     });
     return shareUrl;
 }
+/**
+ alloc方法内部其实就是调用该方法
+ */
++ (instancetype)allocWithZone:(struct _NSZone *)zone
+{
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        shareUrl = [super allocWithZone:zone];
+    });
+    return shareUrl;
+}
+
+/**
+ 防止有人发送copy消息给单例对象
+ */
+- (id)copyWithZone:(NSZone *)zone
+{
+    return self;
+}
+
+
 -(NSString *)urlWithRushBuy{
     return @"http://api.meituan.com/group/v1/deal/activity/1?__skck=40aaaf01c2fc4801b9c059efcd7aa146&__skcy=NF9S7jqv3TVBAoEURoapWJ5VBdQ%3D&__skno=FB6346F3-98FF-4B26-9C36-DC9022236CC3&__skts=1434530933.316028&__skua=bd6b6e8eadfad15571a15c3b9ef9199a&__vhost=api.mobile.meituan.com&ci=1&client=iphone&movieBundleVersion=100&msid=48E2B810-805D-4821-9CDD-D5C9E01BC98A2015-06-17-14-50363&ptId=iphone_5.7&userid=104108621&utm_campaign=AgroupBgroupD100Fab_chunceshishuju__a__a___b1junglehomepagecatesort__b__leftflow___ab_gxhceshi__nostrategy__leftflow___ab_gxhceshi0202__b__a___ab_pindaochangsha__a__leftflow___ab_xinkeceshi__b__leftflow___ab_gxtest__gd__leftflow___ab_gxh_82__nostrategy__leftflow___ab_pindaoshenyang__a__leftflow___i_group_5_2_deallist_poitype__d__d___ab_b_food_57_purepoilist_extinfo__a__a___ab_trip_yidizhoubianyou__b__leftflow___ab_i_group_5_3_poidetaildeallist__a__b___ab_waimaizhanshi__b__b1___a20141120nanning__m1__leftflow___ab_pindaoquxincelue__a__leftflow___ab_i_group_5_5_onsite__b__b___ab_i_group_5_6_searchkuang__a__leftflow&utm_content=4B8C0B46F5B0527D55EA292904FD7E12E48FB7BEA8DF50BFE7828AF7F20BB08D&utm_medium=iphone&utm_source=AppStore&utm_term=5.7&uuid=4B8C0B46F5B0527D55EA292904FD7E12E48FB7BEA8DF50BFE7828AF7F20BB08D&version_name=5.7";
 }
